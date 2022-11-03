70c93100ab2d5478e93746b170c85ed64098a812
ps344 <p.g.sansom@exeter.ac.uk>
2022-11-03 16:48:04 +0000

    Added figures to examples.sh and updated README

--- examples.sh
+++ examples.sh
@@ -5,3 +5,9 @@ Rscript --vanilla identify_fronts.R --diagnostics --nsmooth=2 --thresh_t=-5e-11 
 
 # Run with new parameters
 Rscript --vanilla identify_fronts.R --diagnostics -q data/q850/erai_q850_200001.nc -t data/t850/erai_t850_200001.nc -u data/u850/erai_u850_200001.nc -v data/v850/erai_v850_200001.nc output/final.nc
+
+# Figure 1(b)
+Rscript --vanilla examples/figure1b.R
+
+# Figures 3(b) & 3(c)
+Rscript --vanilla examples/figure3bc.R
